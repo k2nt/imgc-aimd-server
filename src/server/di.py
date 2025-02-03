542e6c9faa02c96023d4fad6fcdd94707358cbef
Khai Nguyen <tuankhai2k@gmail.com>
2025-02-02 20:52:44 -0500

    add resnet50 service to dependency injector

--- src/server/di.py
+++ src/server/di.py
@@ -1,10 +1,15 @@
 from dependency_injector import containers, providers
 
 from server.domain.entity.context import Context
+from server.service.imgc import Resnet50Service
 
 
 class DI(containers.DeclarativeContainer):
     context = providers.Factory(
         Context,
         yaml_path="src/server/ctx.yaml"
+    )
+
+    resnet50_service = providers.Factory(
+        Resnet50Service
     )
--- src/server/service/imgc.py
+++ src/server/service/imgc.py
@@ -1,27 +1,33 @@
 from typing import List
 
 from server.infra.image import preprocess_image_resnet
-from server.infra.imgc import ImgcModel
+from server.infra.imgc import ImgcModel, Resnet50Model
 from server.domain.entity.imgc import Classification
 
 import numpy as np
 
 
 class ImgcService:
+    def __init__(self, model: ImgcModel):
+        self._model = model
+
     def classify(
             self, 
-            model: ImgcModel, 
             image_bytes: bytes
     ) -> Classification:
         image_array = preprocess_image_resnet(image_bytes)
-        results = model.classify(image_array)
+        results = self.model.classify(image_array)
         return results[0]
         
     def classify_batch(
             self, 
-            model: ImgcModel, 
             images_bytes: List[bytes],
     ) -> List[Classification]:
         image_arrays = [preprocess_image_resnet(image_bytes) for image_bytes in images_bytes]
         batch_image_array = np.vstack(image_arrays)
-        return model.classify(batch_image_array)
+        return self.model.classify(batch_image_array)
+
+
+class Resnet50Service:
+    def __init__(self):
+        super().__init__(Resnet50Model())
